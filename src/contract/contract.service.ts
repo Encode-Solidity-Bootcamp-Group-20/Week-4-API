325cebc78b0af71465453e69a655b0bf232afa99
TechnoGecko <cr5697@gmail.com>
2022-07-17 16:38:47 -0700

    Added abi and contract address

--- src/contract/contract.service.ts
+++ src/contract/contract.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { ProviderService } from './../shared/services/provider/provider.service';
+import { ethers } from 'ethers';
+import { SignerService } from './../shared/services/signer/signer.service';
+import * as TokenContract from '../../abi.json';
 
 @Injectable()
 export class ContractService {
-  constructor(private providerService: ProviderService) {}
+  contractPublicInstance: ethers.Contract;
+  contractSignedInstance: ethers.Contract;
+
+  constructor(
+    private providerService: ProviderService,
+    private signerService: SignerService,
+  ) {
+    this.setupContractInstances();
+  }
+
+  setupContractInstances() {
+    const contractAddress = process.env.CONTRACT_ADDRESS;
+    if (!contractAddress || contractAddress.length === 0) return;
+    this.contractPublicInstance = new ethers.Contract(
+      contractAddress,
+      TokenContract.abi,
+      this.providerService.provider,
+    );
+    this.contractSignedInstance = new ethers.Contract(
+      contractAddress,
+      TokenContract.abi,
+      this.signerService.signer,
+    );
+  }
 
   async getNFTMetadataById(id: number) {
     const url =
       'https://ipfs.io/ipfs/QmP5QyW4phUmDEwRh5CwSQTfM3ZrX1TTBmXErmtK3t2mfQ?filename=metadata.json';
   }
+  //How do I query this in node?
 }
--- src/shared/services/signer/signer.service.ts
+++ src/shared/services/signer/signer.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { WalletService } from './../wallet/wallet.service';
+import { ProviderService } from './../provider/provider.service';
+import { ethers } from 'ethers';
 
 @Injectable()
-export class SignerService {}
+export class SignerService {
+  signer: ethers.Wallet;
+
+  constructor(
+    private walletService: WalletService,
+    private providerService: ProviderService,
+  ) {
+    this.setupSigner();
+  }
+
+  setupSigner() {
+    if (!this.walletService.wallet || !this.providerService.provider) return;
+    this.signer = this.walletService.wallet.connect(
+      this.providerService.provider,
+    );
+  }
+}
